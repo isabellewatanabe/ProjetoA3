125dcf9a09b85cd208bfde03a457c1907b907587
isabellewatanabe <bellewatanabe.iw@gmail.com>
2026-06-03 21:03:40 -0300

    cria classe Projeto.java

--- Projeto.java
+++ Projeto.java
@@ -1,28 +1,42 @@
 public class Projeto {
 
-    private String nome;
+    private String nomeProjeto;
     private String descricao;
     private String dataInicio;
-    private String dataFim;
+    private String dataTerminoPrevista;
     private String status;
-    private Usuario gerente;
+    private String gerenteResponsavel;
 
-    public Projeto(String nome, String descricao,
-                   String dataInicio, String dataFim,
-                   String status, Usuario gerente) {
-
-        this.nome = nome;
+    public Projeto(String nomeProjeto, String descricao, String dataInicio, String dataTerminoPrevista, String status, String gerenteResponsavel) {
+        this.nomeProjeto = nomeProjeto;
         this.descricao = descricao;
         this.dataInicio = dataInicio;
-        this.dataFim = dataFim;
+        this.dataTerminoPrevista = dataTerminoPrevista;
         this.status = status;
-        this.gerente = gerente;
+        this.gerenteResponsavel = gerenteResponsavel;
+    }
+
+    public void cadastrarProjeto() {
+        System.out.println("Projeto cadastrado: " + nomeProjeto);
+    }
+
+    public void editarProjeto(String novaDescricao, String novoStatus, String novaDataTerminoPrevista) {
+        this.descricao = novaDescricao;
+        this.status = novoStatus;
+        this.dataTerminoPrevista = novaDataTerminoPrevista;
+        System.out.println("Projeto atualizado: " + nomeProjeto);
+    }
+
+    public void excluirProjeto() {
+        System.out.println("Projeto excluído: " + nomeProjeto);
     }
 
     public void exibirProjeto() {
-        System.out.println("\nPROJETO");
-        System.out.println("Nome: " + nome);
+        System.out.println("Projeto: " + nomeProjeto);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Data de início: " + dataInicio);
+        System.out.println("Data de término prevista: " + dataTerminoPrevista);
         System.out.println("Status: " + status);
-        System.out.println("Gerente: " + gerente.getNome());
+        System.out.println("Gerente responsável: " + gerenteResponsavel);
     }
 }
